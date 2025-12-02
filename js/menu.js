4585c8f704f26760eeb48637a006a4c10f620a24
hermanaaaaaaaa <kcarrillo@centro.edu.mx>
2025-12-01 22:08:05 -0600

    Added changes to edit the scroll bar

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,14 @@
-const menuButton= document.getElementById("button-menu");
-menuButton.addEventListener("click",function(){
-    menuButton.classList.toggle("active")
-})
+// js/menu.js
+
+document.addEventListener("DOMContentLoaded", function () {
+  const buttonMenu = document.getElementById("button-menu");
+  const mainNav = document.getElementById("main-nav");
+
+  if (!buttonMenu || !mainNav) return;
+
+  buttonMenu.addEventListener("click", () => {
+    buttonMenu.classList.toggle("active");
+
+    mainNav.classList.toggle("menu-open");
+  });
+});
